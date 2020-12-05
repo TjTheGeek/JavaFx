6fe06eb1fc9ef1522ba97df6a6429ebf3499bc07
TjTheGeek <Tijesuolalekan@gmail.com>
2020-12-05 18:29:40 +0000

    ---

--- JavafxProject/integrity/SHA256.js
+++
@@ -1,9 +0,0 @@
-const crypto = require('crypto');
-
-const secret = 'abc';
-
-const hash = crypto.createHash("sha256")
-                   .update(secret)
-                   .digest('hex');
-
-console.log(hash);
